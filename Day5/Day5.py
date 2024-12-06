1d3f65c5e239ac9b4ad044e7fdcae7854fbeecfd
Jamillll <mjmister2006@outlook.com>
2024-12-06 18:37:26 +0000

    Day 5 (Part 1 and 2) completed
    This one was so deceiving XD like I read the challenge and it was so long and I wasn't sure how to solve it so I put it off because honestly I was clueless..... I finally started coding- noticed a convenient pattern in the rules that meant I didn't have to actually process them..... and it worked for part 1- kind of felt like cheating..... and then it worked for part 2 and honestly im taking the win

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,5 +1,60 @@
 def main():
-    print()
+    input = open("Day5\input.txt").read().split()
+
+    orderRules = list()
+    updateNumbers = list()
+    for line in input:
+        if line.find("|") != -1:
+            orderRules.append(line)
+        else:
+            updateNumbers.append(line)
+    orderRules.sort()
+
+    correctUpdateTotal = 0
+    resolvedUpdateTotal = 0
+    for update in updateNumbers:
+        update = update.split(",")
+        relevantRules = getRelevantRules(orderRules, update)
+
+        evaluatedUpdate = [None] * len(update)
+        for i, number in enumerate(update):
+            ruleCount = 0
+            for rule in relevantRules:
+                if rule[0] == number:
+                    ruleCount += 1 
+            evaluatedUpdate[ruleCount] = number
+        evaluatedUpdate.reverse()
+
+        print(update)
+        print(evaluatedUpdate)
+        if update == evaluatedUpdate:
+            correctUpdateTotal += int(update[(len(update) // 2)])
+            print("Match!")
+        else:
+            resolvedUpdateTotal += int(evaluatedUpdate[(len(evaluatedUpdate) // 2)])
+
+    print("correct update total: " + str(correctUpdateTotal))
+    print("resolved update total: " + str(resolvedUpdateTotal))
+
+def getRelevantRules(orderRules, updateNumbers):
+    relevantRules = list()
+
+    for rule in orderRules:
+        rule = rule.split("|")
+        hasFoundFirstNumber = False
+        hasFoundSecondNumber = False
+
+        for number in updateNumbers:
+            if number == rule[0]:
+                hasFoundFirstNumber = True
+            if number == rule[1]:
+                hasFoundSecondNumber = True
+
+        if hasFoundFirstNumber and hasFoundSecondNumber:
+            relevantRules.append(rule)
+            continue
+    return relevantRules      
+
 
 if __name__ == "__main__":
     main()
